30a1d3de87b8446de96a1a6a1e9eec92d7bfc6d8
심재천 <shimnaldo98@gmail.com>
2024-08-21 13:44:03 +0900

    feat : 백준 18352번

---
+++ CodingTestCPP/BOJ-18352.cpp
@@ -0,0 +1,32 @@
+// 백준 18352번 특정 거리의 도시 찾기
+
+// 1번부터 N번까지의 도시, M개의 단방향 도로
+
+// 도시 개수, 도로 개수, 거리 정보, 출발 도시의 번호
+// N, M, K, X
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m, k, x;
+vector<int> vec;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m >> k >> x;
+	for (int i = 0; i < m; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		//vec.push_back();
+	}
+
+	return 0;
+}
+
+
