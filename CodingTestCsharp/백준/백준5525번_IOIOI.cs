66ab9a4cb459234ff5972cea5f77ae28e75da7e6
심재천 <shimnaldo98@gmail.com>
2024-10-12 17:23:04 +0900

    백준5525번 : IOIOI (문자열)

---
+++ CodingTestCsharp/백준/백준5525번_IOIOI.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace 백준5525번_IOIOI
+{
+    class Program
+    {
+        static int solution(int n, int m, string s)
+        {
+            int answer = 0;
+            Stack<char> st = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (st.Count == 0)
+                {
+                    if (s[i] == 'I')
+                        st.Push(s[i]);
+                }
+                else
+                {
+                    if (st.Peek() == s[i])
+                    {
+                        while (st.Count > 0) st.Pop();
+
+                        if (s[i] == 'I')
+                            st.Push(s[i]);
+                    }
+                    else
+                    {
+                        if (s[i] == 'I' && st.Count == n * 2)
+                        {
+                            answer++;
+                            // 끝에 IO 만 빼기
+                            st.Pop();
+                            st.Pop();
+                        }
+                        st.Push(s[i]);
+                    }
+                }
+            }
+
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+
+            Console.WriteLine(solution(n, m, s));
+        }
+    }
+
+}
