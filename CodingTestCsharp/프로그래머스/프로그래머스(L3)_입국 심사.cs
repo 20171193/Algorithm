5ce381261b16e7900535bf57c8ed44710a626064
심재천 <shimnaldo98@gmail.com>
2025-01-03 16:37:27 +0900

    프로그래머스(L3) : 입국 심사 (이분 탐색)

---
+++ CodingTestCsharp/프로그래머스/프로그래머스(L3)_입국 심사.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace 프로그래머스_L3_입국_심사
+{
+    public class Solution
+    {
+        static bool Check(int n, long m, int[] times)
+        {
+            long cnt = 0;
+            for (int i = 0; i < times.Length; i++)
+            {
+                cnt += m / times[i];
+                if (cnt >= n)
+                    return true;
+            }
+
+            return false;
+        }
+
+        static long solution(int n, int[] times)
+        {
+            long answer = 0;
+
+            long left = 1, right = 1000000000;
+            long mid = 0;
+            while (left <= right)
+            {
+                mid = (left + right) >> 1;
+
+                if (Check(n, mid, times))
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+
+            answer = left;
+
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(6, new int[2] { 7, 10 }));
+        }
+    }
+}
