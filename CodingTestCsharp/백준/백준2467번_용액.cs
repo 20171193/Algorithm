9b107645bdc98f6d91f10406a5b74a7a7f068667
심재천 <shimnaldo98@gmail.com>
2025-02-27 14:43:54 +0900

    백준2467번 : 용액(투포인터)

---
+++ CodingTestCsharp/백준/백준2467번_용액.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace 백준2467번_용액
+{
+    class Program
+    {
+        static int n;
+        static int[] arr;
+
+        static int[] solution()
+        {
+            int[] answer = new int[2];
+
+            int left = 0, right = n - 1, minDiff = int.MaxValue;
+            while (left < right)
+            {
+                int sum = arr[left] + arr[right];
+                int diff = Math.Abs(sum);
+
+                if (diff < minDiff)
+                {
+                    minDiff = diff;
+                    answer[0] = arr[left];
+                    answer[1] = arr[right];
+                }
+
+                if (sum < 0)
+                    left++;
+                else
+                    right--;
+            }
+
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int[] answer = solution();
+            Console.WriteLine($"{answer[0]} {answer[1]}");
+        }
+    }
+}
