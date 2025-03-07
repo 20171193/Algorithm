74545be131f3e7b88bcde61048d703d07baf757f
심재천 <shimnaldo98@gmail.com>
2025-03-07 22:26:09 +0900

    프로그래머스(L3) : 거스름돈(DP)

---
+++ CodingTestCsharp/프로그래머스/프로그래머스(L3)_거스름돈.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Numerics;
+
+namespace 프로그래머스L3_거스름돈
+{
+    class Program
+    {
+        static int solution(int n, int[] money)
+        {
+            int[] dp = new int[n + 1];
+
+            Array.Sort(money);
+
+            for (int i = 0; i < money.Length; i++)
+            {
+                for (int j = money[i]; j <= n; j++)
+                {
+                    dp[j] += dp[j - money[i]];
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
