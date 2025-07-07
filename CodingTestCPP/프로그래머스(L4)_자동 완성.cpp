abdd31ccf2a592994f84195d84b8fbd711ca7f56
심재천 <shimnaldo98@gmail.com>
2025-07-07 18:42:14 +0900

    프로그래머스(L4) : 자동 완성(트리)

---
+++ CodingTestCPP/프로그래머스(L4)_자동 완성.cpp
@@ -0,0 +1,62 @@
+// 프로그래머스(L4), 카카오(3차) : 자동 완성
+#include <string> 
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+// 사전 순 트리 형태로 구성
+struct Node
+{
+    unordered_map<char, Node*> children;
+    int count = 0;
+    bool isEnd = false;
+};
+
+void Insert(const string& word, Node* root)
+{
+    Node* cur = root;
+    for (char c : word)
+    {
+        auto& children = cur->children;
+        auto it = children.find(c);
+        if (it == children.end())
+        {
+            auto result = children.insert({ c, new Node() });
+            it = result.first;
+        }
+
+        cur = (*it).second;
+        cur->count++;
+    }
+    cur->isEnd = true;
+}
+
+int solution(vector<string> words) {
+    int answer = 0;
+
+    Node* root = new Node();
+
+    sort(words.begin(), words.end());
+    for (const string& w : words)
+        Insert(w, root);
+
+    for (const string& w : words)
+    {
+        Node* cur = root;
+        int cnt = 0;
+
+        for (char c : w)
+        {
+            cur = cur->children[c];
+            cnt++;
+
+            if (cur->count == 1)
+                break;
+        }
+
+        answer += cnt;
+    }
+
+    return answer;
+}
