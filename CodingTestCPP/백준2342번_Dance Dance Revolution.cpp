6dcce2a0226ac8802fe12316a7df65625dd17a73
심재천 <shimnaldo98@gmail.com>
2025-10-25 22:09:22 +0900

    백준 2342번 : Dance Dance Revolution (DP)  : 반례, 코드 오류 수정 (최종 정답 코드)

--- CodingTestCPP/백준2342번_Dance Dance Revolution.cpp
+++ CodingTestCPP/백준2342번_Dance Dance Revolution.cpp
@@ -1,59 +1,67 @@
 // 백준 2342번 : Dance Dance Revolution
-// 연속 : 1, 중앙 : 2, 인접 : 3, 반대 4
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-const int INF = 400003;
+constexpr int INF = 400004;
 
-int arr[1001];
-int dp[1001][5][5];
-
-int GetCost(int cur, int next)
+// 1. 두 발이 같은 지점에 있으면 안됨.
+// 2. 비용
+bool isOpposite(int a, int b)
 {
-	// 연속
-	if (cur == next) return 1;
-	// 중앙
-	if (cur == 0) return 2;
-	// 반대
-	if (abs(next - cur) == 2) return 4;
-	// 인접
-	else return 3;
+	if ((a == 1 && b == 3) || (a == 3 && b == 1)
+		|| (a == 2 && b == 4) || (a == 4 && b == 2))
+		return true;
+	return false;
 }
 
-int solve(int size)
+int GetCost(int start, int end)
+{
+	//  중앙에서 이동 : 2
+	if (start == 0) return 2;
+	//  현재위치 반복 : 1
+	if (start == end) return 1;
+	//  반대 지점 : 4
+	if (isOpposite(start, end))
+		return 4;
+	// 인접 지점
+	return 3;
+}
+
+int solution(const vector<int>& order)
 {
 	int answer = INF;
 
-	fill(&dp[0][0][0], &dp[0][0][0] + (size + 1) * 5 * 5, INF);
+	int n = order.size();
+
+	vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(5, vector<int>(5, INF)));
 	dp[0][0][0] = 0;
 
-	int next;
-	// n-1 반복
-	for (int i = 0; i < size; i++)
+	for (int i = 1; i <= order.size(); i++)
 	{
-		next = arr[i];
+		int next = order[i - 1];
 		for (int l = 0; l < 5; l++)
 		{
 			for (int r = 0; r < 5; r++)
 			{
-				if (dp[i][l][r] == INF) continue;
+				if (dp[i - 1][l][r] == INF) continue;
 
-				// 한 지점에 같은 발x
-				if (next != r)
-					dp[i + 1][next][r] = min(dp[i + 1][next][r], dp[i][l][r] + GetCost(l, next));
 				if (next != l)
-					dp[i + 1][l][next] = min(dp[i + 1][l][next], dp[i][l][r] + GetCost(r, next));
+					dp[i][l][next] = min(dp[i][l][next], dp[i - 1][l][r] + GetCost(r, next));
+				if (next != r)
+					dp[i][next][r] = min(dp[i][next][r], dp[i - 1][l][r] + GetCost(l, next));
 			}
 		}
 	}
 
-	// dp[n][][]
 	for (int l = 0; l < 5; l++)
+	{
 		for (int r = 0; r < 5; r++)
-			answer = min(answer, dp[size][l][r]);
-
+		{
+			if (l == r || dp[n][l][r] == INF) continue;
+			answer = min(answer, dp[n][l][r]);
+		}
+	}
 	return answer;
 }
 
@@ -62,11 +70,16 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int n, idx = 0;
-	while (cin >> n && n != 0)
-		arr[idx++] = n;
+	int n = 0;
+	vector<int> orders;
 
-	cout << solve(idx) << '\n';
+	while (true)
+	{
+		cin >> n;
+		if (n == 0) break;
+		orders.push_back(n);
+	}
 
+	cout << solution(orders);
 	return 0;
 }
