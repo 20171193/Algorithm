1763a2fb776677a94f6a297d6198cabef9f6f21c
심재천 <shimnaldo98@gmail.com>
2025-11-04 21:29:45 +0900

    백준17626번 : Four Squares (DP, 동전문제)

---
+++ CodingTestCsharp/백준/백준17626번_Four Squares.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace 백준17626번_Four_Squares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] dp = new int[n + 1];
+
+            dp[0] = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                dp[i] = int.MaxValue;
+                for (int j = 1; j * j <= i; j++)
+                {
+                    dp[i] = Math.Min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+
+            Console.WriteLine(dp[n]);
+        }
+    }
+}
