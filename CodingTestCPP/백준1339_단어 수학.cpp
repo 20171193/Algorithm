54c39a848a0bf6a3df1b6f5271ff1a09570b49a9
심재천 <shimnaldo98@gmail.com>
2026-03-11 17:07:43 +0900

    백준1339번 : 단어 수학  - 비트마스킹 -> 누적 변경    : 가중치 계산 수정

--- CodingTestCPP/백준1339_단어 수학.cpp
+++ CodingTestCPP/백준1339_단어 수학.cpp
@@ -7,7 +7,7 @@
 // 풀이
 // 1. 그리디
 //  1-1. 자릿수별로 단어 분류
-//  1-2. 단어별로 차수 할당(비트할당 0~8)
+//  1-2. 단어별로 차수 할당 (자릿수)
 //  1-3. 분류된 단어 차수 기준 내림차 정렬
 //  1-4. 높은 자릿수, 높은 차수부터 9~0 치환
 // 2. 10^8 * 10 -> int 불가 long long 사용
@@ -19,27 +19,27 @@
 using namespace std;
 typedef long long ll;
 
-int n, sub[27], degree[27], num = 9;
+int n, num = 9;
+bool used[27];
+ll degree[27];
 string words[10];
 
 ll solution() {
-	fill(sub, sub + 27, -1);
-
 	vector<vector<int>> digitNum(9);
 
 	// 차수 할당, 자릿수별 문자 할당
 	for (int i = 0; i < n; i++) {
 		int digit = 0;
 		for (auto iter = words[i].rbegin(); iter != words[i].rend(); iter++, digit++) {
-			int ascii = (*iter) - '0';
-			if (degree[ascii] & (1 << digit)) continue;
+			int ascii = (*iter) - 'A';
 
-			degree[ascii] |= (1 << digit);
+			degree[ascii] += pow(10, digit);
 			digitNum[digit].push_back(ascii);
 		}
 	}
 
-	// 문자 치환 
+	// 문자 치환 + 결과 누적
+	ll result = 0;
 	for (auto iter = digitNum.rbegin(); iter != digitNum.rend(); iter++) {
 		if ((*iter).empty()) continue;
 
@@ -47,20 +47,11 @@ ll solution() {
 		sort((*iter).begin(), (*iter).end(), [](int a, int b) { return degree[a] > degree[b]; });
 
 		for (int ascii : (*iter)) {
-			if (sub[ascii] != -1) continue;
-			sub[ascii] = num--;
+			if (!used[ascii]){
+				result += ascii * degree[ascii];
+				used[ascii] = true;
+			}
 		}
-	}
-
-	// 단어 덧셈
-	ll result = 0;
-	for (int i = 0; i < n; i++) {
-		int num = 0;
-		int digit = 0;
-		for (auto iter = words[i].rbegin(); iter != words[i].rend(); iter++, digit++) {
-			num += sub[(*iter) - '0'] * pow(10, digit);
-		}
-		result += num;
 	}
 
 	return result;
